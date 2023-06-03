4c3d05d31838f36c14a7df11f521cdcef2484b06
JohnWay <1973372075@qq.com>
2023-06-03 21:36:49 +0800

    Signed-off-by: JohnWay <1973372075@qq.com>

---
+++ button/examples/example_button.c
@@ -0,0 +1,17 @@
+/**
+ * @file example_button.c
+ * @author JohnWay (1973372075@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-03
+ * 
+ * @copyright Copyright (c) 2023 JohnWay
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ */
+#include "button.h"
+
+void button_example()
+{
+    
+}
---
+++ button/inc/button.h
@@ -0,0 +1,15 @@
+/**
+ * @file button.h
+ * @author JohnWay (1973372075@qq.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-06-03
+ * 
+ * @copyright Copyright (c) 2023 JohnWay
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ */
+#ifndef BUTTON_H
+#define BUTTON_H
+
+#endif /* BUTTON_H */
